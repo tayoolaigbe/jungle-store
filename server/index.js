5644e522f7377cb0f55ccc6cd6fe103dff6766e8
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2021-07-09 01:20:24 +0100

    Working on resolvers

--- server/index.js
+++ server/index.js
@@ -1,52 +1,75 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { mainCards, animals, categories } = require('./db');
 
 const typeDefs = gql`
-	type Book {
-		title: String
-		author: String
-	}
-
 	type MainCard {
 		title: String!
 		image: String!
 	}
 
+	type Animal {
+		id: ID!
+		image: String!
+		title: String!
+		rating: Float
+		price: String
+		description: [String!]!
+		stock: Int!
+		onSale: Boolean
+		slug: String!
+		category: Category
+	}
+
+	type Category {
+		id: ID!
+		image: String!
+		category: String!
+		slug: String!
+		animals: [Animal!]!
+	}
+
 	type Query {
-		books: [Book]
 		mainCards: [MainCard]
+		animals: [Animal!]!
+		animal(slug: String!): Animal
+		categories: [Category!]!
+		category(slug: String!): Category
 	}
 `;
 
-const books = [
-	{
-		title: 'The Awakening',
-		author: 'Kate Chopin',
-	},
-	{
-		title: 'City of Glass',
-		author: 'Paul Auster',
-	},
-];
-
-const mainCards = [
-	{
-		title: 'Recently Viewed',
-		image: 'Lion',
-	},
-	{
-		title: 'Looking for a Gift',
-		image: 'Penguin',
-	},
-	{
-		title: 'Best Behaved',
-		image: 'Cat',
-	},
-];
-
 const resolvers = {
 	Query: {
-		books: () => books,
 		mainCards: () => mainCards,
+		animals: () => animals,
+		animal: (parent, args, ctx) => {
+			let animal = animals.find(animal => {
+				return animal.slug === args.slug;
+			});
+			return animal;
+		},
+		categories: () => categories,
+		category: (parent, args, ctx) => {
+			let category = categories.find(category => {
+				return category.slug === args.slug;
+			});
+			return category;
+		},
+	},
+	Category: {
+		animals: (parent, args, ctx) => {
+			let result = animals.filter(animal => {
+				return animal.category === parent.id;
+			});
+			return result;
+		},
+	},
+	Animal: {
+		category: (parent, args, ctx) => {
+			let result = categories.find(category => {
+				return category.id === parent.category;
+			});
+			return result;
+		},
 	},
 };
 
